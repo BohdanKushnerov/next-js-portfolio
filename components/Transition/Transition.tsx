3e8d2dacbe42c84502abaebb073038145b7e6f9f
Bohdan Kushnerov <bohdan.kushnerov@gmail.com>
2023-11-17 09:36:01 +0200

    add page services

--- components/Transition/Transition.tsx
+++ components/Transition/Transition.tsx
@@ -1,20 +1,7 @@
 import React, { FC } from "react";
 import { motion } from "framer-motion";
 
-const transitionVariants = {
-  initial: {
-    x: "100%",
-    width: "100%",
-  },
-  animate: {
-    x: "0%",
-    width: "0%",
-  },
-  exit: {
-    x: ["0%", "100%"],
-    width: ["0%", "100%"],
-  },
-};
+import { transitionVariants } from "@constants/transitionVariants";
 
 export const Transition: FC = () => {
   return (
